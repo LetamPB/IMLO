ce81631b4676c007490f42059adc6badb4190181
Letam <letam4nigeria@gmail.com>
2025-05-22 21:24:55 +0100

    CNN set up

--- train.py
+++ train.py
@@ -18,11 +18,13 @@ batch_size = 64
 training_set = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform
 )
-
+# to have consistent split
+generator = torch.Generator().manual_seed(42)
 # splitting dataset between training(45k) and validation(5k)
 train_size = 45000
 val_size = 5000
-train_dataset, val_dataset = random_split(training_set, [train_size, val_size])
+train_dataset, val_dataset = random_split(
+    training_set, [train_size, val_size], generator=generator)
 
 # loaders for training and validation
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
@@ -34,6 +36,24 @@ validation_loader = DataLoader(
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
+        # dataset images are RGB and thus have 3 input channels
+        # padding added to avoid misisng edge of image
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)  # pooling layer helps wiht downsampling
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        # fully connected
+        self.fc1 = nn.Linear(128 * 8 * 8, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 10)
 
     def forward(self, x):
-        pass
+        x = self.pool(torch.relu(self.conv1(x)))  # conv1 -> ReLU -> pool
+        x = self.pool(torch.relu(self.conv2(x)))  # conv2 -> ReLU -> pool
+        x = torch.flatten(x, 1)               # flatten for linear layer
+        x = torch.relu(self.fc1(x))               # fc1 -> ReLU
+        x = torch.relu(self.fc2(x))               # fc2 -> ReLU
+        x = self.fc3(x)                       # final output layer
+        return x
